242b0973379cbc77e4ff8b7fafae4785e80c1518
Lynn Gabbay <gabbay@gmail.com>
2015-10-18 18:05:11 -0700

    added nesting and recipe-wide configurations

--- foostitch.py
+++ foostitch.py
@@ -62,6 +62,44 @@ def usage():
     sys.exit(1)
 
 
+def parse_recipe(recipes, name, context, scripts, inputs):
+    if not isinstance(name, basestring):
+        raise TypeError("name must be a basestring")
+    if not isinstance(context, dict):
+        raise TypeError("context must be a dict")
+    if not isinstance(scripts, list):
+        raise TypeError("scripts must be a list")
+    if not isinstance(inputs, list):
+        raise TypeError("inputs must be a list")
+
+    if name not in recipes:
+        raise ValueError("recipe {} not found".format(name))
+    sequence = recipes[name]
+    if not isinstance(sequence, list):
+        raise TypeError("recipe {} not a list".format(name))
+
+    i = 0
+    while i < len(sequence):
+        item = sequence[i]
+        i = i + 1
+        if isinstance(item, dict):
+            context.update(item)
+        elif isinstance(item, basestring):
+            if item.startswith("*"):
+                parse_recipe(recipes, item[1:], copy.deepcopy(context), scripts, inputs)
+            else:
+                scripts.append(item)
+                if (i < len(sequence)) and isinstance(sequence[i], dict):
+                    data = copy.deepcopy(context)
+                    data.update(sequence[i])
+                    inputs.append(data)
+                    i = i + 1
+                else:
+                    inputs.append(copy.deepcopy(context))
+        else:
+            return ValueError("unexpected item in recipe")
+
+
 def parse_command_line():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "d:f:o:c:", [
@@ -93,20 +131,13 @@ def parse_command_line():
 
     recipes = load_configuration_file(cfg.configuration_file)
 
-    for s in recipes.get(cfg.recipe_name, []):
-        if isinstance(s, dict):
-            assert len(cfg.scripts) == len(cfg.inputs) + 1
-            cfg.inputs.append(s)
-        elif isinstance(s, (str, unicode)):
-            if len(cfg.inputs) < len(cfg.scripts):
-                cfg.inputs.append({})
-            assert len(cfg.scripts) == len(cfg.inputs)
-            cfg.scripts.append(s)
-        else:
-            raise ValueError()
+    if cfg.recipe_name not in recipes:
+        raise ValueError("recipe {} not found".format(cfg.recipe_name))
 
-    if len(cfg.inputs) < len(cfg.scripts):
-        cfg.inputs.append({})
+    context = {}
+    parse_recipe(recipes, cfg.recipe_name, context, cfg.scripts, cfg.inputs)
+
+    assert len(cfg.inputs) == len(cfg.scripts)
 
     return cfg
 
