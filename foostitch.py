12a53c468b2dd78212ab2604452e2d61d3dc224c
Lynn Gabbay <gabbay@gmail.com>
2018-12-05 12:36:22 -0800

    cleaned up and improved parsing configuration files

--- foostitch.py
+++ foostitch.py
@@ -30,15 +30,17 @@ def _load_configuration_file(*args):
     return result
 
 
-def _parse_recipe(recipes, name, default_context, templates, contexts):
+def _parse_recipe(recipes, name, base_context, templates, contexts):
+    if not isinstance(recipes, dict):
+        raise TypeError("recipes must be a dict")
     if not isinstance(name, basestring):
         raise TypeError("name must be a basestring")
-    if not isinstance(default_context, dict):
-        raise TypeError("context must be a dict")
+    if not isinstance(base_context, dict):
+        raise TypeError("base_context must be a dict")
     if not isinstance(templates, list):
-        raise TypeError("scripts must be a list")
+        raise TypeError("templates must be a list")
     if not isinstance(contexts, list):
-        raise TypeError("inputs must be a list")
+        raise TypeError("contexts must be a list")
 
     if name not in recipes:
         raise ValueError("recipe {} not found".format(name))
@@ -46,24 +48,32 @@ def _parse_recipe(recipes, name, default_context, templates, contexts):
     if not isinstance(sequence, list):
         raise TypeError("recipe {} not a list".format(name))
 
-    i = 0
-    while i < len(sequence):
+    # if the first entry is a dict, overlay the base context for the recipe
+    if (len(sequence) != 0) and isinstance(sequence[0], dict):
+        recipe_base_context = copy.deepcopy(base_context)
+        recipe_base_context.update(sequence[0])
+        i = 1
+    else:
+        recipe_base_context = base_context
+        i = 0
+
+    while i != len(sequence):
         item = sequence[i]
         i = i + 1
-        if isinstance(item, dict):
-            default_context.update(item)
-        elif isinstance(item, basestring):
-            if item.startswith("*"):
-                _parse_recipe(recipes, item[1:], copy.deepcopy(default_context), templates, contexts)
+        if isinstance(item, basestring):
+            if (i != len(sequence)) and isinstance(sequence[i], dict):
+                item_context = copy.deepcopy(recipe_base_context)
+                item_context.update(sequence[i])
+                i = i + 1
             else:
+                item_context = recipe_base_context
+            if item.startswith("*"):
+                # include this recipe
+                _parse_recipe(recipes, item[1:], item_context, templates, contexts)
+            else:
+                # include this template with optional context
                 templates.append(item)
-                if (i < len(sequence)) and isinstance(sequence[i], dict):
-                    data = copy.deepcopy(default_context)
-                    data.update(sequence[i])
-                    contexts.append(data)
-                    i = i + 1
-                else:
-                    contexts.append(copy.deepcopy(default_context))
+                contexts.append(item_context)
         else:
             return ValueError("unexpected item in recipe")
 
@@ -98,13 +108,13 @@ class Session(object):
             raise ValueError("recipe {} not found".format(self.recipe_name))
 
         if "*" in recipes:
-            context = recipes["*"]
-            if not isinstance(context, dict):
+            base_context = recipes["*"]
+            if not isinstance(base_context, dict):
                 raise ValueError("default context must be a dict")
         else:
-            context = {}
+            base_context = {}
 
-        _parse_recipe(recipes, self.recipe_name, context, self._templates, self._contexts)
+        _parse_recipe(recipes, self.recipe_name, base_context, self._templates, self._contexts)
 
         assert len(self._contexts) == len(self._templates)
 
